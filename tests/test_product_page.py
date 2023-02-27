1a9c1484245a5d398d13f81e73bff62216882c91
Kristina <kristina_1651@mail.ru>
2023-02-27 19:51:58 +0300

    Initial commit

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -3,7 +3,6 @@ import time
 
 from page_objects.ProductsPage import ProductsPage
 from page_objects.MainPage import MainPage
-from page_objects.CartPage import CartPage
 
 def test_check_elements_books_page(browser):
     # Переход на страницу Books
@@ -38,23 +37,6 @@ def test_check_elements_books_page(browser):
     elements_filter = ProductsPage(browser).search_elements_filter_select()
     assert "Under 25.00" and "25.00 - 50.00" and "Over 50.00" in elements_filter
     print(elements_filter)
-
-def test_add_product_to_shopping_cart(browser):
-    # Переход на страницу Books
-    ProductsPage(browser).open('https://demowebshop.tricentis.com')
-    MainPage(browser).click_categories_books()
-    # Название товара (первый в каталоге)
-    link_product = ProductsPage(browser).get_product_link(0)
-    # Добавить товар в корзину (первый товар в списке)
-    ProductsPage(browser).click_button_add_to_cart(0)
-    # Проверка уведомления
-    MainPage(browser).verify_message('The product has been added to your shopping cart')
-    # Зайти в корзину
-    MainPage(browser).click_shopping_cart()
-    # Проверка, что товар добавлен в корзину
-    link_product_shopping_cart = CartPage(browser).check_link_product_shopping_cart()
-    assert link_product == link_product_shopping_cart
-
 
 def test_check_full_product_cart(browser):
     ProductsPage(browser).open('https://demowebshop.tricentis.com')
