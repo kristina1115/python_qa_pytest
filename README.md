668eb07462a09244c8e42974cbf9d1cbd9770f42
Kristina <kristina_1651@mail.ru>
2023-03-05 16:57:49 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_qa_pytest
+В данном проекте используется Python 3.8
--- page_objects/MainPage.py
+++ page_objects/MainPage.py
@@ -2,7 +2,6 @@ from locators.Main import Main
 from .BasePage import BasePage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
 
